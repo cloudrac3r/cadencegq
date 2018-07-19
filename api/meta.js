53c34e3a940a8ec2fe9ac267d5781eba2c76d906
Edward Fish <cloudrac3r@vivaldi.net>
2018-07-19 23:23:26 +1200

    Add public hit count API

--- api/meta.js
+++ api/meta.js
@@ -1,3 +1,5 @@
+const cf = require("../util/common.js");
+
 module.exports = ({db}) => {
     return [
         {
@@ -7,6 +9,19 @@ module.exports = ({db}) => {
                 output.nodeVersion = process.version;
                 output.requests = (await db.get("SELECT SUM(hits) AS hits FROM Hits")).hits;
                 return [200, output];
+            }
+        },
+        {
+            route: "/api/hits", methods: ["GET"], code: async () => {
+                let rows = await db.all("SELECT * FROM Hits ORDER BY hits DESC");
+                rows = rows.slice(0, 6);
+                let columns = [[], []];
+                for (let row of rows) {
+                    columns[1].push(Object.values(row)[0]);
+                    columns[0].push(Object.values(row)[1]);
+                }
+                let table = cf.tableify(columns, ["right", "left"]);
+                return [200, table];
             }
         }
     ]
