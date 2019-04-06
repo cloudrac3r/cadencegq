e72cda3868ac586ccbba7cf0b67abbc1ccd177ea
Cadence Fish <cloudrac3r@vivaldi.net>
2019-04-06 23:53:58 +1300

    Revise terms and privacy, and update DB schema

--- db/main-schema.sql
+++ db/main-schema.sql
@@ -1,4 +1,5 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS `URLs`;
 CREATE TABLE IF NOT EXISTS `URLs` (
 	`hash`	TEXT,
 	`authorAccount`	INTEGER,
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `URLs` (
 	`expiryTime`	INTEGER,
 	PRIMARY KEY(`hash`)
 );
+DROP TABLE IF EXISTS `Pastes`;
 CREATE TABLE IF NOT EXISTS `Pastes` (
 	`pasteID`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`authorAccount`	INTEGER,
@@ -16,11 +18,30 @@ CREATE TABLE IF NOT EXISTS `Pastes` (
 	`creationTime`	INTEGER,
 	`expiryTime`	INTEGER
 );
+DROP TABLE IF EXISTS `DiscordQuotes`;
+CREATE TABLE IF NOT EXISTS `DiscordQuotes` (
+	`id`	TEXT,
+	`messageID`	TEXT,
+	`channelID`	TEXT,
+	`guildID`	TEXT,
+	`content`	TEXT,
+	`author`	TEXT,
+	`image`	TEXT,
+	PRIMARY KEY(`id`)
+);
+DROP TABLE IF EXISTS `DomainHits`;
+CREATE TABLE IF NOT EXISTS `DomainHits` (
+	`domain`	TEXT,
+	`hits`	INTEGER,
+	PRIMARY KEY(`domain`)
+);
+DROP TABLE IF EXISTS `InvidiousServers`;
 CREATE TABLE IF NOT EXISTS `InvidiousServers` (
 	`country`	TEXT,
 	`prefix`	TEXT,
 	PRIMARY KEY(`country`)
 );
+DROP TABLE IF EXISTS `Images`;
 CREATE TABLE IF NOT EXISTS `Images` (
 	`imageID`	TEXT,
 	`extension`	TEXT,
@@ -29,16 +50,19 @@ CREATE TABLE IF NOT EXISTS `Images` (
 	`expiryTime`	INTEGER,
 	PRIMARY KEY(`imageID`)
 );
+DROP TABLE IF EXISTS `Hits`;
 CREATE TABLE IF NOT EXISTS `Hits` (
 	`url`	TEXT,
 	`hits`	INTEGER,
 	PRIMARY KEY(`url`)
 );
+DROP TABLE IF EXISTS `Godmaster`;
 CREATE TABLE IF NOT EXISTS `Godmaster` (
 	`userID`	INTEGER,
 	`summary`	TEXT,
 	PRIMARY KEY(`userID`)
 );
+DROP TABLE IF EXISTS `BingoTiles`;
 CREATE TABLE IF NOT EXISTS `BingoTiles` (
 	`id`	INTEGER,
 	`cardid`	INTEGER,
@@ -48,16 +72,19 @@ CREATE TABLE IF NOT EXISTS `BingoTiles` (
 	`y2`	INTEGER,
 	PRIMARY KEY(`id`)
 );
+DROP TABLE IF EXISTS `BingoTagsMap`;
 CREATE TABLE IF NOT EXISTS `BingoTagsMap` (
 	`cardid`	INTEGER,
 	`tagid`	INTEGER,
 	PRIMARY KEY(`cardid`,`tagid`)
 );
+DROP TABLE IF EXISTS `BingoTags`;
 CREATE TABLE IF NOT EXISTS `BingoTags` (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	INTEGER,
 	`description`	TEXT
 );
+DROP TABLE IF EXISTS `BingoCards`;
 CREATE TABLE IF NOT EXISTS `BingoCards` (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT,
@@ -65,18 +92,21 @@ CREATE TABLE IF NOT EXISTS `BingoCards` (
 	`external`	TEXT,
 	`author`	TEXT
 );
+DROP TABLE IF EXISTS `Accounts`;
 CREATE TABLE IF NOT EXISTS `Accounts` (
 	`userID`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`username`	TEXT UNIQUE,
 	`hash`	TEXT,
 	`salt`	TEXT
 );
+DROP TABLE IF EXISTS `AccountTokens`;
 CREATE TABLE IF NOT EXISTS `AccountTokens` (
 	`userID`	INTEGER,
 	`token`	TEXT UNIQUE,
 	`expires`	INTEGER,
 	PRIMARY KEY(`userID`,`token`)
 );
+DROP TABLE IF EXISTS `AccountSubscriptions`;
 CREATE TABLE IF NOT EXISTS `AccountSubscriptions` (
 	`userID`	TEXT,
 	`channelID`	TEXT,
