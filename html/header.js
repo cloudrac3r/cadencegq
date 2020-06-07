c3c10028f45d2f9ab3b551acb5a86863b33fad89
Cadence Ember <cloudrac3r@vivaldi.net>
2020-06-07 18:21:10 +1200

    Make header keyboard accessible

--- html/header.js
+++ html/header.js
@@ -5,25 +5,25 @@ if (document.readyState == "loading") {
 }
 
 function makeHeadersWork() {
-    let dropdowns = [...document.querySelectorAll("#header > .headerArrow")];
+    let dropdowns = [...document.querySelectorAll("#header .headerArrow")];
     function dropdownToDiv(element) {
-        return element.parentElement.children[[...element.parentElement.children].indexOf(element)-1];
+        return element.parentElement.children[[...element.parentElement.children].indexOf(element)+1];
     }
-    window.addEventListener("mousedown", event => {
+    window.addEventListener("click", event => {
         //console.log(event);
         if (event.target.tagName == "A" && event.target.classList.contains("headerButton")) return;
         dropdowns.forEach(e => dropdownToDiv(e).style.display = "none");
     });
     dropdowns.forEach(element => {
         let divElement = dropdownToDiv(element);
-        element.addEventListener("mousedown", () => {
-            if (divElement.style.display != "inline-block") setTimeout(() => {
-                if (divElement.style.display != "inline-block") divElement.style.display = "inline-block";
+        element.addEventListener("click", () => {
+            if (divElement.style.display != "block") setTimeout(() => {
+                if (divElement.style.display != "block") divElement.style.display = "block";
                 else divElement.style.display = "none";
             });
         });
     });
-    document.querySelector("#headerExpandMenu").addEventListener("mousedown", () => {
+    document.querySelector("#headerExpandMenu").addEventListener("click", () => {
         document.querySelector("#header").classList.toggle("hidden");
     });
 }
