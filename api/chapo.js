1987530b20463f6feb03f0fcb8e48cf1103d0518
Cadence Ember <cadence@disroot.org>
2021-01-02 14:06:37 +1300

    Migrate chapo.chat emojis AGAIN

--- api/chapo.js
+++ api/chapo.js
@@ -11,38 +11,13 @@ module.exports = [
 				return emojis
 			} else {
 				emojisCached = Date.now()
-				return emojis = (async () => {
-					async function fetchEmojis(page) {
-						const url = `https://gitlab.com/api/v4/projects/19692201/repository/tree?path=ui/public/emojis&per_page=100&page=${page}`
-						const res = await fetch(url, {
-							headers: {
-								"PRIVATE-TOKEN": pat
-							}
-						})
-						const headers = res.headers
-						const files = await res.json()
-						return {
-							headers,
-							files
-						}
-					}
-
-					const first = await fetchEmojis(1)
-					let result = first.files
-					const totalPages = first.headers.get("X-Total-Pages")
-					if (totalPages > 1) {
-						result = result.concat(
-							...await Promise.all(
-								Array(totalPages-1).fill().map((_, i) => fetchEmojis(i+2).then(x => x.files))
-							)
-						)
-					}
+				return emojis = fetch("https://git.chapo.chat/api/v1/repos/chapo-collective/hexbear-frontend/contents/public/emojis/").then(res => res.json()).then(root => {
 					return {
 						statusCode: 200,
 						contentType: "application/json",
-						content: result
+						content: root
 					}
-				})()
+				})
 			}
 		}
 	}
